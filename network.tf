faf4a4a921307706624b66ef1b94b87e2b39f3ad
dalyjarose <69650350+dalyjarose@users.noreply.github.com>
2024-10-21 09:10:46 -0400

    Network
    Vnet and subnets defined

---
+++ network.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  subscription_id = "7aa42b38-3d5e-4711-b3b0-fde054e92142" # This is the subscription for my own use, demo purposes only. Your subscription ID would go here
+  features {}
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "challenge-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sub1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "sub2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "sub3" {
+  name                 = "subnet3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "sub4" {
+  name                 = "subnet4"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
